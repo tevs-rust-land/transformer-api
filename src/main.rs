3dc3e9ac61026f992573cdd49a71e74f166c81ac
Tevinthuku <tevinthuku@gmail.com>
2020-06-15 14:42:30 +0300

    incorporate 400 on the gostruct to flow  endpoint

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ extern crate rocket_contrib;
 #[macro_use]
 extern crate serde_derive;
 
+use rocket::response::status::BadRequest;
+use rocket::Request;
 use rocket_contrib::json::{Json, JsonValue};
 
 #[derive(Serialize, Deserialize)]
@@ -16,13 +18,15 @@ struct TransformRequest {
 }
 
 #[post("/gostruct/to/flow", format = "json", data = "<data>")]
-fn transform_go_struct_to_flow(data: Json<TransformRequest>) -> JsonValue {
+fn transform_go_struct_to_flow(
+    data: Json<TransformRequest>,
+) -> Result<JsonValue, BadRequest<JsonValue>> {
     match js_typify_gostruct::transform(data.contents.to_string()) {
-        Ok(res) => json!({ "data": res }),
-        Err(parse_error) => json!({
+        Ok(res) => Ok(json!({ "data": res })),
+        Err(parse_error) => Err(BadRequest(Some(json!({
             "status": "error",
             "reason": format!("{:?}", parse_error)
-        }),
+        })))),
     }
 }
 
@@ -31,8 +35,17 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[catch(404)]
+fn not_found(req: &Request) -> JsonValue {
+    json!({
+        "status": "error",
+        "reason": format!("Sorry, '{}' is not a valid path.", req.uri())
+    })
+}
+
 fn main() {
     rocket::ignite()
         .mount("/api/v1", routes![index, transform_go_struct_to_flow])
+        .register(catchers![not_found])
         .launch();
 }

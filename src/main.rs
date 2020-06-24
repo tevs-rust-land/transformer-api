ae5453ff5601f0685e34a50a9d863f14cccf87bc
Tevinthuku <tevinthuku@gmail.com>
2020-06-24 10:16:56 +0300

    fix issue with rocket cors by using attach method & not the manage method

--- src/main.rs
+++ src/main.rs
@@ -11,40 +11,9 @@ use rocket::http::Method;
 
 use rocket::response::status::BadRequest;
 use rocket_contrib::json::{Json, JsonValue};
+use rocket_cors::{AllowedHeaders, AllowedOrigins, Error};
 
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::{ContentType, Header};
-use rocket::{Request, Response};
-use std::io::Cursor;
-
-pub struct CORS();
-
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to requests",
-            kind: Kind::Response,
-        }
-    }
-
-    fn on_response(&self, request: &Request, response: &mut Response) {
-        if request.method() == Method::Options || response.content_type() == Some(ContentType::JSON)
-        {
-            response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-            response.set_header(Header::new(
-                "Access-Control-Allow-Methods",
-                "POST, GET, OPTIONS",
-            ));
-            response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
-            response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-        }
-
-        if request.method() == Method::Options {
-            response.set_header(ContentType::Plain);
-            response.set_sized_body(Cursor::new(""));
-        }
-    }
-}
+use rocket::Request;
 
 #[derive(Serialize, Deserialize)]
 struct TransformRequest {
@@ -77,21 +46,6 @@ fn transform_go_struct_to_typescript(
     }
 }
 
-#[options("/gostruct/to/flow")]
-fn optionflowroute() -> &'static str {
-    "Hello, world!"
-}
-
-#[options("/gostruct/to/typescript")]
-fn optiontypescriptroute() -> &'static str {
-    "Hello, world!"
-}
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
 #[catch(404)]
 fn not_found(req: &Request) -> JsonValue {
     json!({
@@ -100,19 +54,30 @@ fn not_found(req: &Request) -> JsonValue {
     })
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
+    let allowed_origins = AllowedOrigins::All;
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get, Method::Post, Method::Options]
+            .into_iter()
+            .map(From::from)
+            .collect(),
+        allowed_headers: AllowedHeaders::All,
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()?;
     rocket::ignite()
         .mount(
             "/api/v1",
             routes![
-                index,
                 transform_go_struct_to_flow,
-                transform_go_struct_to_typescript,
-                optionflowroute,
-                optiontypescriptroute
+                transform_go_struct_to_typescript
             ],
         )
         .register(catchers![not_found])
-        .attach(CORS())
+        .attach(cors)
         .launch();
+
+    Ok(())
 }
